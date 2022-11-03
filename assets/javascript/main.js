1304ef477014b5d5fe5bb04fedcba0a059755010
john65 <joaovini2667@gmail.com>
2022-11-03 16:46:51 -0300

    version 1.0

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,24 +1,30 @@
-let input = document.getElementById("myInput");
+/*==VARIABLES==*/
+let input = document.getElementById('input');
 let listArr = [];
+
+/*==KEYBOARD ACTIVATED EVENT==*/
 input.addEventListener('keyup', (e) => {
-    if(e.keyCode === 13){  // enter keyboard
-        if(input.value != ''){
+
+    /*==EVENT ACTIVATED BY THE ENTER KEY==*/
+    if (e.keyCode === 13) {
+        if (input.value != '') {
             let inValue = e.target.value;
             listArr.push(inValue.replace(/\s/g, ''));
             let newTagLi = '';
             listArr.forEach((element, index) => {
                 newTagLi += `<span># ${element}<i class="fa fa-times" onclick="ondelete(${index})"></i></span>`;
-            }) ;
+            });
             document.querySelector('.tags').innerHTML = newTagLi;
             input.value = '';
-        }else{
-            alert("Please input something");
+        } else {
+            alert('Please input something')
         }
     }
 })
 
-function ondelete(index){
-    listArr.splice(index, 1);
+/*==FUCNTION TO REMOVE TAG==*/
+function ondelete(index) {
+    listArr.splice(index, 1)
     let newTagLi = '';
     listArr.forEach((element, index) => {
         newTagLi += `<span># ${element}<i class="fa fa-times" onclick="ondelete(${index})"></i></span>`;
